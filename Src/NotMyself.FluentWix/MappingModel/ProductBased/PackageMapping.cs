dc5c6d3cd61a12e410a3d844c5f373662a66a86f
Ang3lFir3 <ang3lfir3@gmail.com>
2010-07-21 22:29:10 -0700

    Added naive implementation of the required Package node of Product

---
+++ Src/NotMyself.FluentWix/MappingModel/ProductBased/PackageMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NotMyself.FluentWix.Visitors;
+
+namespace NotMyself.FluentWix.MappingModel.ProductBased
+{
+    public class PackageMapping : MappingBase
+    {
+        public override void AcceptVisitor(IMappingModelVisitor visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsSpecified(string property)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
